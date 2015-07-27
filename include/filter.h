7f4ba5343e24a4730a7df8b8fc465b30a8834c89
Dave Cridland <dave.cridland@surevine.com>
2015-07-27 14:04:59 +0100

    Some filtering work [f]

---
+++ include/filter.h
@@ -0,0 +1,52 @@
+#ifndef METRE_FILTER__H
+#define METRE_FILTER__H
+
+#include "defs.h"
+
+#include <map>
+#include <string>
+#include <memory>
+#include <set>
+
+namespace Metre {
+  class Filter {
+  public:
+    class BaseDescription {
+    protected:
+      BaseDescription(std::string && name);
+    public:
+      std::string const name;
+    protected:
+      std::set<std::string> m_suppress_features;
+      std::set<std::string> m_namespaces;
+    };
+    template<typename T> class Description {
+    public:
+      Description(std::string && name);
+      bool suppress_feature(std::string const & feature);
+
+      std::unique_ptr<T> create(XMLStream &);
+    };
+  protected:
+    static std::multimap<std::string, BaseDescription *> & all_filters();
+  public:
+    Filter(XMLStream &);
+    virtual bool apply(bool inbound, Stanza &) = 0;
+
+    virtual ~Filter();
+
+    template<typename T> static bool declare(const char * name) {
+      BaseDescription * bd = new typename T::Description(name);
+      for (auto & ns : bd->m_namespaces) {
+        all_filters().insert(std::make_pair(ns,bd));
+      }
+      return true;
+    }
+    static void instantiate(std::string const & xmlns, XMLStream &);
+
+  protected:
+    XMLStream & m_stream;
+  };
+}
+
+#endif
---
+++ src/filter.cc
@@ -0,0 +1,9 @@
+#include "filter.h"
+#include "xmlstream.h"
+#include "stanza.h"
+
+using namespace Metre;
+
+void Filter::instantiate(std::string const & xmlns, XMLStream &) {
+  // Do nothing.
+}
