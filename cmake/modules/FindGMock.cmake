94ad7d64792b6f5c3a0370ae5a9a406ded77c616
Pete Woods <pete.woods@surevine.com>
2019-04-05 19:48:02 +0100

    Get (mostly) non-vendored OSX builds working
    - Depends on https://github.com/Homebrew/homebrew-core/pull/38737
    - Updated vendored version of googletest to release 1.8.1

--- cmake/modules/FindGMock.cmake
+++ cmake/modules/FindGMock.cmake
@@ -28,65 +28,31 @@
 # NOTE: Due to the way this package finder is implemented, do not attempt
 # to find the GMock package more than once.
 
-find_package(Threads)
-
-if (EXISTS "/usr/src/googletest")
-    # As of version 1.8.0
-    set(GMOCK_SOURCE_DIR "/usr/src/googletest/googlemock" CACHE PATH "gmock source directory")
+if(DEFINED GOOGLETEST_ROOT_DIR)
+    set(GMOCK_SOURCE_DIR "${GOOGLETEST_ROOT_DIR}/googlemock" CACHE PATH "gmock source directory")
     set(GMOCK_INCLUDE_DIRS "${GMOCK_SOURCE_DIR}/include" CACHE PATH "gmock source include directory")
-    set(GTEST_INCLUDE_DIRS "/usr/src/googletest/googletest/include" CACHE PATH "gtest source include directory")
+    set(GTEST_SOURCE_DIR "${GOOGLETEST_ROOT_DIR}/googletest" CACHE PATH "gmock source directory")
+    set(GTEST_INCLUDE_DIRS "${GTEST_SOURCE_DIR}/include" CACHE PATH "gtest source include directory")
+elseif(EXISTS "/usr/src/googletest")
+    # As of version 1.8.0
+    set(GOOGLETEST_ROOT_DIR "/usr/src/googletest")
+    set(GMOCK_SOURCE_DIR "${GOOGLETEST_ROOT_DIR}/googlemock" CACHE PATH "gmock source directory")
+    set(GMOCK_INCLUDE_DIRS "${GMOCK_SOURCE_DIR}/include" CACHE PATH "gmock source include directory")
+    set(GTEST_SOURCE_DIR "${GOOGLETEST_ROOT_DIR}/googletest" CACHE PATH "gmock source directory")
+    set(GTEST_INCLUDE_DIRS "${GTEST_SOURCE_DIR}/include" CACHE PATH "gtest source include directory")
 else()
-    set(GMOCK_SOURCE_DIR "/usr/src/gmock" CACHE PATH "gmock source directory")
+    set(GOOGLETEST_ROOT_DIR "/usr/src/gmock" CACHE PATH "gmock source directory")
     set(GMOCK_INCLUDE_DIRS "/usr/include" CACHE PATH "gmock source include directory")
     set(GTEST_INCLUDE_DIRS "/usr/include" CACHE PATH "gtest source include directory")
 endif()
 
-# We add -g so we get debug info for the gtest stack frames with gdb.
-# The warnings are suppressed so we get a noise-free build for gtest and gmock if the caller
-# has these warnings enabled.
-set(findgmock_cxx_flags "${CMAKE_CXX_FLAGS} -g -Wno-old-style-cast -Wno-missing-field-initializers -Wno-ctor-dtor-privacy -Wno-switch-default")
+# Prevent GoogleTest from overriding our compiler/linker options
+# when building with Visual Studio
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 
-set(findgmock_bin_dir "${CMAKE_CURRENT_BINARY_DIR}/gmock")
-set(findgmock_gtest_lib "${findgmock_bin_dir}/gtest/libgtest.a")
-set(findgmock_gtest_main_lib "${findgmock_bin_dir}/gtest/libgtest_main.a")
-set(findgmock_gmock_lib "${findgmock_bin_dir}/libgmock.a")
-set(findgmock_gmock_main_lib "${findgmock_bin_dir}/libgmock_main.a")
+add_subdirectory("${GOOGLETEST_ROOT_DIR}" "${CMAKE_BINARY_DIR}/googletest" EXCLUDE_FROM_ALL)
 
-include(ExternalProject)
-ExternalProject_Add(GMock SOURCE_DIR "${GMOCK_SOURCE_DIR}"
-                          BINARY_DIR "${findgmock_bin_dir}"
-                          BUILD_BYPRODUCTS "${findgmock_gtest_lib}"
-                                           "${findgmock_gtest_main_lib}"
-                                           "${findgmock_gmock_lib}"
-                                           "${findgmock_gmock_main_lib}"
-                          INSTALL_COMMAND ""
-                          CMAKE_ARGS "-DCMAKE_CXX_FLAGS=${findgmock_cxx_flags}")
-
-add_library(gtest INTERFACE)
-target_include_directories(gtest INTERFACE ${GTEST_INCLUDE_DIRS})
-target_link_libraries(gtest INTERFACE ${findgmock_gtest_lib} ${CMAKE_THREAD_LIBS_INIT})
-add_dependencies(gtest GMock)
-
-add_library(gtest_main INTERFACE)
-target_include_directories(gtest_main INTERFACE ${GTEST_INCLUDE_DIRS})
-target_link_libraries(gtest_main INTERFACE ${findgmock_gtest_main_lib} gtest)
-
-add_library(gmock INTERFACE)
-target_include_directories(gmock INTERFACE ${GMOCK_INCLUDE_DIRS})
-target_link_libraries(gmock INTERFACE ${findgmock_gmock_lib} gtest)
-
-add_library(gmock_main INTERFACE)
-target_include_directories(gmock_main INTERFACE ${GMOCK_INCLUDE_DIRS})
-target_link_libraries(gmock_main INTERFACE ${findgmock_gmock_main_lib} gmock)
-
-set(GTEST_LIBRARIES gtest)
-set(GTEST_MAIN_LIBRARIES gtest_main)
-set(GMOCK_LIBRARIES gmock gmock_main)
-set(GTEST_BOTH_LIBRARIES ${GTEST_LIBRARIES} ${GTEST_MAIN_LIBRARIES})
-
-unset(findgmock_cxx_flags)
-unset(findgmock_bin_dir)
-unset(findgmock_gtest_lib)
-unset(findgmock_gtest_main_lib)
-unset(findgmock_gmock_lib)
-unset(findgmock_gmock_main_lib)
+set(GTEST_LIBRARIES gtest CACHE PATH "gtest libraries")
+set(GTEST_MAIN_LIBRARIES gtest_main CACHE PATH "gtest libraries")
+set(GMOCK_LIBRARIES gmock gmock_main CACHE PATH "gtest libraries")
+set(GTEST_BOTH_LIBRARIES ${GTEST_LIBRARIES} ${GTEST_MAIN_LIBRARIES} CACHE PATH "gtest libraries")
