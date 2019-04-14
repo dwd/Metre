fd562726d65daac6f88cddfcb326d7e3ad0be2c8
Dave Cridland <dave@cridland.net>
2019-04-14 16:11:40 +0100

    Need to disable declaration too

--- include/sigslot.h
+++ include/sigslot.h
@@ -5,12 +5,14 @@
 #ifndef METRE_SIGSLOT_H
 #define METRE_SIGSLOT_H
 
+#ifdef SIGSLOT_RESUME_OVERRIDE
 #include <experimental/coroutine>
 #include "core.h"
 
 namespace sigslot {
     void resume(std::experimental::coroutine_handle<> coro);
 }
+#endif
 
 #include <sigslot/sigslot.h>
 
