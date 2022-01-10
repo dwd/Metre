11c37df87f5ff6071bc28cd427fa02839de61247
Dave Cridland <dave@forwardhealth.co>
2022-01-10 15:08:31 +0000

    Tidy up CoThread

--- include/cothread.h
+++ include/cothread.h
@@ -12,20 +12,79 @@
 namespace Metre {
     template<typename Result, class... Args>
     class CoThread {
-        sigslot::signal<Result> m_completed;
-        std::function<Result()> m_fn;
+    public:
+        // Single argument version uses a bare T
+        struct awaitable {
+            std::coroutine_handle<> awaiting = nullptr;
+            CoThread & wait_for;
+
+            explicit awaitable(CoThread & t) : wait_for(t) {}
+
+            bool await_ready() {
+                return wait_for.has_payload();
+            }
+
+            void await_suspend(std::coroutine_handle<> h) {
+                // The awaiting coroutine is already suspended.
+                awaiting = h;
+                wait_for.await(this);
+            }
+
+            auto await_resume() {
+                return wait_for.payload();
+            }
+
+            void resolve() {
+                std::coroutine_handle<> a = nullptr;
+                std::swap(a, awaiting);
+                if (a) sigslot::resume(a);
+            }
+        };
+    private:
+        std::function<Result(Args...)> m_fn;
+        std::optional<std::jthread> m_thread;
+        std::optional<Result> m_payload;
+        std::recursive_mutex m_mutex;
+        awaitable * m_awaitable = nullptr;
 
     public:
-        CoThread(std::function<Result()> && fn) : m_fn(fn) {}
 
-        sigslot::tasklet<Result> run(Args&&... args) {
-            auto wrapped_fn = [cothread=this](Args... a) {
-                auto result = cothread->m_fn(a...);
-                cothread->m_completed(result);
+        explicit CoThread(std::function<Result(Args...)> && fn) : m_fn(std::move(fn)) {}
+
+        CoThread & run(Args&&... args) {
+            auto wrapped_fn = [this](Args... a) {
+                auto result = m_fn(a...);
+                {
+                    std::lock_guard l_(m_mutex);
+                    m_payload.emplace(result);
+                }
+                if (m_awaitable) {
+                    m_awaitable->resolve();
+                }
             };
-            std::jthread thread(wrapped_fn, args...);
-            auto result = co_await m_completed;
-            co_return result;
+            m_thread.emplace(wrapped_fn, args...);
+            return *this;
+        }
+
+        awaitable operator co_await() {
+            return awaitable(*this);
+        }
+
+        bool has_payload() {
+            std::lock_guard l_(m_mutex);
+            return m_payload.has_value();
+        }
+
+        auto payload() {
+            return *m_payload;
+        }
+
+        void await(awaitable * a) {
+            std::lock_guard l_(m_mutex);
+            m_awaitable = a;
+            if (m_payload.has_value()) {
+                a->resolve();
+            }
         }
     };
 }
